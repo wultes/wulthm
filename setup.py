8a156259806ea34ca69f717595c19adbb5e11eff
Кирилл Мельников <wultes@MacBook-Air-Kirill.local>
2022-06-13 23:25:06 +0500

    Feat(wulthm): Implement 1.0 version

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="wulthm",
+    version="1.0",
+    license="MIT",
+    author="Kirill Melnikov @wultes",
+    packages=find_packages(),
+    url="https://github.com/wultes/wulthm"
+)
